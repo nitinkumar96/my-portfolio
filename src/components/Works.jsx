35e383ad9f5e54d24e9bd84b14b70fcd187848cc
Nitin Kumar <nitinkumar06031@gmail.com>
2024-02-10 00:37:46 +0530

    Undo remove tilt

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,55 +8,6 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const Project = ({
-  name,
-  description,
-  tags,
-  image,
-  source_code_link,
-}) => {
-  return (
-    <>
-      <div className='relative w-full h-[230px]'>
-        <img
-          src={image}
-          alt='project_image'
-          className='w-full h-full object-cover rounded-2xl'
-        />
-
-        <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-          <div
-            onClick={() => window.open(source_code_link, "_blank")}
-            className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-          >
-            <img
-              src={github}
-              alt='source code'
-              className='w-1/2 h-1/2 object-contain'
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className='mt-5'>
-        <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-        <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-      </div>
-
-      <div className='mt-4 flex flex-wrap gap-2'>
-        {tags.map((tag) => (
-          <p
-            key={`${name}-${tag.name}`}
-            className={`text-[14px] ${tag.color}`}
-          >
-            #{tag.name}
-          </p>
-        ))}
-      </div>
-    </>
-  )
-}
-
 const ProjectCard = ({
   index,
   name,
@@ -65,14 +16,8 @@ const ProjectCard = ({
   image,
   source_code_link,
 }) => {
-
-  const isMobile = window.innerWidth <= 768;
-
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      {isMobile ? (
-        <Project name={name} description={description} tags={tags} image={image} source_code_link={source_code_link} />
-      ) : (
       <Tilt
         options={{
           max: 45,
@@ -81,9 +26,43 @@ const ProjectCard = ({
         }}
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
-        <Project name={name} description={description} tags={tags} image={image} source_code_link={source_code_link} />
+        <div className='relative w-full h-[230px]'>
+          <img
+            src={image}
+            alt='project_image'
+            className='w-full h-full object-cover rounded-2xl'
+          />
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
+
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
       </Tilt>
-      )}
     </motion.div>
   );
 };
